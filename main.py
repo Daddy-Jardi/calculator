fa3bd1de2663bd4601c5b2c61bf3d9b558da0eb1
Daddy-Jardi <jardielgaming@gmail.com>
2024-01-10 21:08:06 -0500

    Multiply function added

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ def subtract():
     answer = num1 - num2
     print(f'The difference of {num1} and {num2} is equal to {answer}')
 
+def multiply():
+    num1 = float(input('What is the first number? '))
+    num2 = float(input('What is the second number? '))
+    answer = num1 * num2
+    print(f'The multiplication of {num1} and {num2} is equal to {answer}')
+
 operation = input('What math operation would you like to do? ')
 
 if operation == 'add':
     add()
 elif operation == 'subtract':
     subtract()
+elif operation == 'multiply':
+    multiply()
 
 
 
